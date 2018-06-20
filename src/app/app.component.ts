eead74d96266144c58589c54a58c9d6b52691767
Toni Nemtisor <nemtisor.antonio@gmail.com>
2018-06-20 19:03:15 +0300

    v 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  nume: string;
+  age: number;
+  gasit: boolean;
+
+  constructor(private httpClient: HttpClient) { }
+  
+  onKeyUp(event: any) {
+    this.nume = event.target.value;
+    this.gasit = false;
+  }
+
+  arataProfil() {
+    this.httpClient.get(`http://my-json-server.typicode.com/metonel/json-faker/useri/?name=${this.nume}`).subscribe(
+      (data:any[]) => {
+        if(data.length) {
+          this.age = data[0].age;
+          this.gasit = true;
+        }
+      }
+    )
+  }
+
+  salveazaProfil() {
+    this.httpClient.post(`http://my-json-server.typicode.com/metonel/json-faker/useri/`, 
+  {
+    nume: 'Mark',
+    age: "41"
+  }).subscribe(
+      (data:any) => {
+        console.log(data);
+        }
+    )
+  }
+
 }
